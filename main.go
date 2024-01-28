edef8665db73e324195666b0b4d5e0c22f31115c
Victor Broman <victor.bo.broman@gmail.com>
2024-01-28 22:44:20 +0100

    initial

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import "fmt"
+
+// A HTTP servers function is to allow the user to receive http requests and send http responses
+// It does this by using some underlying communication protocoll e.g. TCP.
+// A core concept in this domain is the URL - Unified Resource locator. The requests specify a URL which specifies a resource.
+// Another core conecpt is the HTTP method which indiciates intent.
+// The combination of the URL and the method is used to select different behaviours which can modify state of the server as well as response.
+// Another concept is Headers which contains meta-data about the request and responses
+
+type URL string
+
+type Header string
+
+type HttpMethod string
+
+const (
+	GET   HttpMethod = "GET"
+	POST  HttpMethod = "POST"
+	PUT   HttpMethod = "PUT"
+	DELET HttpMethod = "DELETE"
+)
+
+type Port int
+
+type Response string
+
+type Request struct {
+	url        URL
+	httpMethod HttpMethod
+	headers    []Header
+}
+
+type Route struct {
+	url    URL
+	method HttpMethod
+}
+type Controller = func(req Request, res Response) Response
+
+type Config struct {
+}
+
+type routeController struct {
+	route      Route
+	controller Controller
+}
+
+type Server struct {
+	port             Port
+	routeControllers []struct {
+		route      Route
+		controller Controller
+	}
+	addController func(route Route, controller Controller)
+	listen        func(port Port)
+	handleRequest func(res Request) Response
+}
+
+func initServer(config Config) Server {
+	var server Server
+
+	server.addController = func(route Route, controller Controller) {
+		server.routeControllers = append(
+			server.routeControllers,
+			routeController{
+				route:      route,
+				controller: controller,
+			})
+	}
+
+	server.handleRequest = func(req Request) Response {
+		return "response"
+	}
+
+	server.listen = func(port Port) {
+		fmt.Printf("Listening on port: %d\n", port)
+		var requestUrl URL
+		for {
+			fmt.Print("Make request: ")
+			fmt.Scan(&requestUrl)
+			fmt.Println(server.handleRequest(Request{url: requestUrl, httpMethod: GET}))
+		}
+	}
+
+	return server
+}
+
+func main() {
+	server := initServer(Config{})
+	server.addController(
+		Route{url: "/", method: GET},
+		func(req Request, res Response) Response {
+			res = "response"
+			return res
+		},
+	)
+	server.listen(3333)
+}
